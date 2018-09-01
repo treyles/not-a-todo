6bc85cc742297192f98bf6d4069ecd56861e7ef0
Troy Reyles <troyreyles@gmail.com>
2018-08-31 17:31:57 -0700

    finished styles

---
+++ public/scripts/main.js
@@ -0,0 +1,40 @@
+$(function() {
+  // DELETE request
+  $('.delete-btn').click(e => {
+    const id = $(e.target).attr('data-id');
+    $.ajax({
+      url: `/todos/${id}`,
+      type: 'DELETE',
+      success: () => {
+        window.location.href = '/';
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  });
+
+  // PUT request
+  $('.completed-btn').click(e => {
+    $target = $(e.target);
+    $target.toggleClass('completed');
+    const id = $target.attr('data-id');
+    $.ajax({
+      url: `/todos/update/${id}`,
+      type: 'PUT',
+      success: () => {
+        window.location.href = '/';
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  });
+
+  $('header h5').html(
+    moment()
+      .format('MMM D')
+      .toUpperCase()
+  );
+  $('header h1').html(moment().format('dddd'));
+});
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 const router = require('express').Router();
 const Todo = require('../models/todo');
 
+// GET todos
 router.get('/', (req, res) => {
   Todo.find({})
     .then(results => {
       res.render('index', {
-        todos: results.reverse()
+        todos: results
       });
     })
     .catch(err => console.log(err));
 });
 
+// NEW todo
 router.post('/todos', (req, res) => {
+  // if (!req.body.description.length) {
+  //   res.redirect('/');
+  //   return;
+  // }
+
   let newTodo = new Todo({
     description: req.body.description
   });
@@ -19,7 +26,7 @@ router.post('/todos', (req, res) => {
   newTodo
     .save()
     .then(result => {
-      console.log(result);
+      // console.log(result);
       res.redirect('/');
     })
     .catch(err => {
@@ -28,16 +35,43 @@ router.post('/todos', (req, res) => {
     });
 });
 
-router.post('/todos/:id', (req, res) => {
-  console.log(req.params.id);
+// DELETE todo
+router.delete('/todos/:id', (req, res) => {
   const todoId = req.params.id;
 
   Todo.findByIdAndRemove(todoId)
     .then(result => {
-      console.log(result);
-      res.redirect('/');
+      res.send(result);
     })
     .catch(err => console.log(err));
+});
+
+// DELETE completed
+// router.delete('/completed', (req, res) => {
+//   Todo.deleteMany({
+//     done: true
+//   })
+//     .then(result => {
+//       res.send(result);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// });
+
+// UPDATE todo
+router.put('/todos/update/:id', (req, res) => {
+  const todoId = req.params.id;
+
+  Todo.findById(todoId)
+    .exec()
+    .then(result => {
+      result.done = !result.done;
+      return result.save();
+    })
+    .then(response => {
+      res.send(response);
+    });
 });
 
 module.exports = router;
